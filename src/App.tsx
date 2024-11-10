ac757026ef428705f7d2439f402668356580d9d1
hasan-tec <hasan.anas.yt@gmail.com>
2024-11-10 21:44:30 +0500

    deployement ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { motion, AnimatePresence } from "framer-motion"
+import { motion} from "framer-motion"
 import { Header } from "@/components/Header"
 import { QueryInput } from "@/components/QueryInput"
 import { QuickQuestions } from "@/components/QuickQuestions"
